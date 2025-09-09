0fd4f5411aa5a96757843e0c87172424dc986bad
Leo Rocca <lrocca@google.com>
2025-09-09 14:17:47 -0300

    Update var

--- landing-project.tf
+++ landing-project.tf
@@ -8,7 +8,7 @@ module "project" {
   source = "git@github.com:leorocca-gg/modules.git//project?ref=main"
   billing_account = var.billing_account.id
   name            = "landing-prj"
-  parent          = "folders/146457895233"
+  parent          = var.folder_ids.networking
   prefix          = var.prefix
   services = [
     "compute.googleapis.com",
