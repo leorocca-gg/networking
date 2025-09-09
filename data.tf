adc67ffd5c9470bef350f537b99eb83b0c3706ff
Leo Rocca <lrocca@google.com>
2025-09-09 18:28:10 -0300

    Config networks

--- data.tf
+++ data.tf
@@ -10,16 +10,18 @@ data "google_compute_network" "vpc-ulan1" {
 data "google_compute_subnetwork" "sn-ulan1" {
   name   = "sn-ulan1-usc1-a"
   region = "us-central1"
+  project = "chspoc-landing-prj"  
 }
 
 ### ULAN 1
 
-data "google_compute_network" "vpc-2" {
+data "google_compute_network" "vpc-ulan2" {
   name = "vpc-ulan2"
   project = "chspoc-landing-prj"
 }
 
 data "google_compute_subnetwork" "sn-ulan2" {
-  name   = "sn-ulan1-use4-a"
+  name   = "sn-ulan2-use4-a"
   region = "us-east4"
+  project = "chspoc-landing-prj"
 }
