f5143c367039e2098aa4e4fa9e22adef06b5f952
Leo Rocca <lrocca@google.com>
2025-09-12 18:36:25 -0300

    Change fw policies path

--- net-global-fw_policies.tf
+++ net-global-fw_policies.tf
@@ -14,8 +14,8 @@ module "net-firewall-policy" {
   }
   factories_config = {
     cidr_file_path          = var.factories_config.firewall.cidr_file
-    egress_rules_file_path  = "${var.factories_config.firewall.policy_rules}"
-    ingress_rules_file_path = "${var.factories_config.firewall.policy_rules}"
+    egress_rules_file_path  = "${var.factories_config.firewall.hierarchical.egress_rules}"
+    ingress_rules_file_path = "${var.factories_config.firewall.hierarchical.ingress_rules}"
   }
   #security_profile_group_ids = var.security_profile_groups
 }
